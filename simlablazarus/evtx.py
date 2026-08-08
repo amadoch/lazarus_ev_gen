dcbb94b0208d77e874a455fafad8e17bb6079059
amadoch <abec2003@hotmail.com>
2026-08-08 13:15:30 -0400

    Generación de evtx

--- simlablazarus/evtx.py
+++ simlablazarus/evtx.py
@@ -1,39 +1,28 @@
 #Generador de Evtx
-import logging, hashlib, random, datetime
+import logging
+
+from xml2evtx import create_evtx, process_xml_file
+
 from simlablazarus import artifact
 
 module_logger = logging.getLogger("lab_lazarus.evtx")
 
-def generar_archivo_evtx(nombre_evtx, directorio):
+def generar_archivo_evtx(xml_file, nombre_evtx, directorio):
     """
     Genera un archivo .evtx binario sintético y correlacionado con los eventos del sistema
     (como EventID 4624, 7045, 4672, 1102) para su análisis con evtx_dump o python-evtx.
     """
     artMan = artifact.ArtifactManager()
+
+    # Procesa xml y convierte datos evtx
+    total_event_count, total_chunk_count, evtx_chunk = process_xml_file(xml_file)
+
+    # Crea los datos de archivo evtx
+    evtx_data = create_evtx(evtx_chunk, total_event_count, total_chunk_count)
+
     ruta_archivo = artMan.create_file(nombre_evtx, directorio)
+
+    with ruta_archivo.open("wb", encoding="utf-8") as f:
+        f.write(evtx_data)
+
     
-    try:
-        # Cabecera estándar de archivos EVTX de Windows ("ElfFile\x00")
-        evtx_header = b"ElfFile\x00" + b"\x00" * 512
-        
-        salt_evtx = random.randint(100, 999)
-        evtx_body = (
-            f"LAZARUS_EVTX_STREAM_{nombre_evtx}_ID_CORRELATION_"
-            f"SEC_SYS_APP_SESSION_{salt_evtx}"
-        ).encode('utf-8')
-        
-        evtx_padding = 4096 - len(evtx_header) - len(evtx_body)
-        if evtx_padding < 0:
-            evtx_padding = 0
-            
-        evtx_binario = evtx_header + evtx_body + (b"\x00" * evtx_padding)
-        
-        with open(ruta_archivo, "wb") as f:
-            f.write(evtx_binario)
-            
-        sha256_evtx = hashlib.sha256(evtx_binario).hexdigest()
-        module_logger.info(f"Registro EVTX correlacionado: {nombre_evtx:<35} | SHA256: {sha256_evtx}")
-        return sha256_evtx
-    except IOError as e:
-        module_logger.error(f"No se pudo escribir el archivo EVTX {nombre_evtx}: {e}")
-        return None
